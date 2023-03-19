51384bb28131cc1122f6c5105585f4cd60785d97
alcho01 <74156727+alcho01@users.noreply.github.com>
2023-03-19 16:48:52 -0400

    Working On Assets

--- Assets/scripts/PlayerInteraction.cs
+++ Assets/scripts/PlayerInteraction.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    PlayerControls controls;
+
     [SerializeField] Text LevelCompleteText;
+
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Player.Interact.performed += ctx => InteractionOn();
+    }
 
     void Start()
     {
@@ -13,26 +24,29 @@ public class PlayerInteraction : MonoBehaviour
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
-            float interactRange = 2f;
-            Collider[] colliderArray = Physics.OverlapSphere (transform.position, interactRange);
-            foreach(Collider collider in colliderArray) {
 
-                if (collider.TryGetComponent(out FridgeInteraction fridgeInteraction))
-                {
+    }
 
-          
-                    fridgeInteraction.Interact();
-                }
+    void InteractionOn()
+    {
+        float interactRange = 2f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+        foreach (Collider collider in colliderArray)
+        {
 
-                if (collider.TryGetComponent(out StoveInteraction stoveInteraction)) {
-                    stoveInteraction.Interact();
+            if (collider.TryGetComponent(out FridgeInteraction fridgeInteraction))
+            {
 
-                    LevelComplete();
-                    Time.timeScale = 0f;
-                }
 
-               
+                fridgeInteraction.Interact();
+            }
+
+            if (collider.TryGetComponent(out StoveInteraction stoveInteraction))
+            {
+                stoveInteraction.Interact();
+
+                LevelComplete();
+                Time.timeScale = 0f;
             }
         }
     }
@@ -40,5 +54,15 @@ public class PlayerInteraction : MonoBehaviour
     public void LevelComplete()
     {
         LevelCompleteText.gameObject.SetActive(true);
+    }
+
+    void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Player.Disable(); 
     }
 }
